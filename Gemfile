79eda926d9c4ce75b959a2ba1b5a1dc07e6382e3
David BT <davidbtbt@users.noreply.github.com>
2020-06-26 08:51:53 -0700

    swagg

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'api-pagination'
 gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'pg'
+gem 'swagger-docs'
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem 'rack-cors'
 gem 'validates_email_format_of'
--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,23 +1,23 @@
-# # frozen_string_literal: true
-#
-# module Swagger
-#   module Docs
-#     # Swagger Config
-#     class Config
-#       def self.transform_path(path, api_version)
-#         # Makes the API docs path distinct
-#         "apidocs/#{path}"
-#       end
-#     end
-#   end
-# end
-#
-# Swagger::Docs::Config.register_apis(
-#   {
-#     '1.0': {
-#       controller_base_path: '',
-#       api_file_path: 'public/apidocs',
-#       clean_directory: true
-#     }
-#   }
-# )
+# frozen_string_literal: true
+
+module Swagger
+  module Docs
+    # Swagger Config
+    class Config
+      def self.transform_path(path, api_version)
+        # Makes the API docs path distinct
+        "apidocs/#{path}"
+      end
+    end
+  end
+end
+
+Swagger::Docs::Config.register_apis(
+  {
+    '1.0': {
+      controller_base_path: '',
+      api_file_path: 'public/apidocs',
+      clean_directory: true
+    }
+  }
+)
